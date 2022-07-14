80ed932d72933e507693f3e731b163ea91b9ab65
Andrii Osadchuk <phantomkiev@icloud.com>
2022-07-14 20:33:45 +0300

    merge hw_3

--- src/homeWork/index.js
+++
@@ -1,9 +0,0 @@
-let userAge = +prompt('Enter your year of birth');
-let userName = prompt('Enter your first name');
-let userSurname = prompt('Enter your surname');
-
-const year = 2022;
-
-let age = year - userAge;
-
-console.log('User: ' + userName + ' ' + userSurname + ', ' + age + ' years old;');
